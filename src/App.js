8d97144b47f89e50489ba6e1dad4093f8c3fc47f
Muhammad Jafar <muhammadjafarrahimjonzoda@gmail.com>
2024-01-21 21:42:18 +0300

    another step

--- src/App.js
+++ src/App.js
@@ -1,45 +1,25 @@
+import React, { Fragment } from 'react';
 import { useState } from 'react';
 import './App.css';
+import Data from './Data';
 
 function App() {
-	const [inputValue,setInputValue] = useState("");
-	const [array,setArray] = useState([]);
-
-	const handleClick=(e)=>{
-		setInputValue(e.target.value);
-	}
-	const handleKey= (e)=>{
-		if(e.key === 'Enter'){
-			setToArray(inputValue);
-			setInputValue("");
-		}
-	}
-	const setToArray = (valueOf)=>{
-		const newArr = [...array,{value:valueOf,id:array.length,done:false}];
-		setArray(newArr);
-		console.log("Heeey");
-	}
-	const deleteIt = (e)=>{
-		const AnotherArray = array.filter((elem)=> elem !== e);
-		// const newArray = myArray.filter((elem) => elem !== elementToDelete);
-		setArray(AnotherArray);
-		// console.log("Hhbjn");
-	}
-
     return (
       	<div className="App">
-			<input onKeyDown={handleKey} 
-					value={inputValue} 
-					onChange={handleClick} 
-					className='inputEdits' 
-					placeholder='Type something and enter'
-					type='text'>
-			</input>
-			<div>
-				{array.map((elem)=>(
-					<h3 key={elem.id}>{elem.value} <input onClick={()=>deleteIt(elem)} type='checkbox'></input></h3>
-				))}
-			</div>
+			<section className='mainBlock'>
+				<h1 className='title'>Fast Food Service24</h1>
+					{Data.map((elem)=>(
+						<div className="fragment" key={elem.id}>
+							<div className='imageBlock'>
+								<img className='imageItself' src={elem.image}/>
+							</div>
+							<div className='textBlock'>
+								<h4>{elem.name}</h4>
+								<p>{elem.price}</p>
+							</div>
+						</div>
+					))}
+			</section>
       	</div>
     );
 }
---
+++ src/Data.js
@@ -0,0 +1,24 @@
+import Burger from './images/Burger2.avif';
+import Pizza from './images/pizza2.avif';
+import Lavash from './images/Lavash2.jpg';
+  const Data = [
+    {
+        id:1,
+        name:"Burger",
+        price:78,
+        image:Burger,
+    },
+    {
+        id:2,
+        name:"Pizza",
+        price:108,
+        image:Pizza,
+    },
+    {
+      id:3,
+      name:"Lavash",
+      price:98,
+      image:Lavash,
+  }
+  ]
+export default Data;
